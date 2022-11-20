8e0eec6ba91bdb845c2aaa8161864ecc313d0ecc
alex2young <20201035+alex2young@users.noreply.github.com>
2022-11-20 14:30:26 -0600

    refine data model

--- src/ledger.rs
+++ src/ledger.rs
@@ -1,29 +1,31 @@
-use crate::model::{Account, Amount, Transaction};
+use crate::model::{Account, Amount, Currency, Transaction};
+use rust_decimal_macros::dec;
 use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct Ledger {
-    account_balances: HashMap<String, Amount>,
+    account_balances: HashMap<Account, Amount>,
 }
 
 impl Ledger {
     pub fn process(&mut self, transactions: Vec<Transaction>) {
         for txn in transactions.into_iter() {
-            for e in txn.entries() {
-                let account = e.account();
-                let currency = e.amount().currency();
-                if !self.account_balances.contains_key(account.name()) {
-                    self.account_balances
-                        .insert(String::from(account.name()), Amount::new(*currency));
+            for posting in txn.postings() {
+                let account = posting.account();
+                let currency = posting.amount().currency();
+                if !self.account_balances.contains_key(account) {
+                    self.account_balances.insert(
+                        Account::from(account),
+                        Amount::new(dec!(0), Currency::from(currency)),
+                    );
                 }
-                let amount = self.account_balances.get_mut(account.name()).unwrap();
-                amount.set_number(amount.number() + e.amount().number());
+                let amount = self.account_balances.get_mut(account).unwrap();
+                amount.plus(posting.amount());
             }
         }
     }
 
     pub fn get_balance(&self, account: &Account) -> Option<&Amount> {
-        println!("{:?}", self.account_balances);
-        self.account_balances.get(account.name())
+        self.account_balances.get(account)
     }
 }
